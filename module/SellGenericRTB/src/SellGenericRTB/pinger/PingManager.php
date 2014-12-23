29611395be039f38219d444e0ca956315d13d5d6
baonh <nhbao.la@gmail.com>
2014-12-23 23:52:48 +0700

    debug

--- module/SellGenericRTB/src/SellGenericRTB/pinger/PingManager.php
+++ module/SellGenericRTB/src/SellGenericRTB/pinger/PingManager.php
@@ -264,7 +264,7 @@ class PingManager {
 		$logger = \rtbsellv22\RtbSellV22Logger::get_instance();
 		$OpenRTBWorkflow = new \sellrtb\workflows\OpenRTBWorkflow();
 
-		var_dump($AuctionPopo);
+
 		 
 		$this->winning_partner_pinger = $OpenRTBWorkflow->process_business_rules_workflow($logger, $this->config, $this->RTBPingerList, $AuctionPopo);
 
